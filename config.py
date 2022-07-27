8238432a69e2a4745ba24e67cdd9bf9728733b5d
lovelydaemon <slamy777@gmail.com>
2022-07-27 19:40:54 +0300

    added creation for db_uri

--- config.py
+++ config.py
@@ -1,8 +1,22 @@
-import os
-from dotenv import load_dotenv
-load_dotenv(verbose=True)
+from dotenv import dotenv_values
+
+config = dotenv_values('.env', verbose=True)
+
+def create_db_uri(config: dict[str, str | None]) -> str:
+    """Return the db URI, default sqlite."""
+    base = config.get('DB_BASE', 'sqlite')
+    user = config.get('DB_USER')
+    host = config.get('DB_HOST', 'localhost')
+    name = config.get('DB_NAME', 'db_name')
+
+    if base == 'sqlite':
+        return f'{base}:///{name}'
+
+    if not user:
+        raise ValueError('Required user login and password')
+    return f'{base}://{user}@{host}/{name}'
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY', 'default_key')
+    SQLALCHEMY_DATABASE_URI = create_db_uri(config)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
